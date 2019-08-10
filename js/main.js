82d68336aadd05bb09983cd5fc79475bf16008da
nacim harfouche <nacim_4@hotmail.fr>
2019-08-10 08:03:47 +0200

    add some visual style (css), clean the code (js)

--- js/main.js
+++ js/main.js
@@ -4,28 +4,23 @@ addButtonElt = document.getElementById("addButton"),
 listElt = document.getElementById("list"),
 clearAllButtonElt = document.getElementById("clearAllButton");
 
-// create a counter
-let count = 1;
-
 // when i click on the ADD button
 addButtonElt.addEventListener("click", () => {
     // create a li element and set attributes
     let li = document.createElement("li");
-    //li.id = `myId${count}`;
 
     // first way how it's possible to do
-    li.textContent = entryElt.value;
-    // create a button 
+    li.textContent = `${entryElt.value} `;
+    // create a button with his attributes
     let buttonRemove = document.createElement("button");
     buttonRemove.type = "button";
     buttonRemove.classList.add("deleteButton");
-    //buttonRemove.id = count;
-    // create an icon
+    // create an icon with his attributes
     let iconElt = document.createElement("i");
     iconElt.classList.add("fas", "fa-times");
 
     // second way to do
-    // li.innerHTML = `${entryElt.value} <button type="button" id="${count}" class="deleteButton"><i class="fas fa-times"></i></button>`;
+    // li.innerHTML = `${entryElt.value} <button type="button" class="deleteButton"><i class="fas fa-times"></i></button>`;
 
     // append 
     // i in button
@@ -34,19 +29,23 @@ addButtonElt.addEventListener("click", () => {
     li.appendChild(buttonRemove);
     // li in ul
     listElt.appendChild(li);
-    count++;
 });
 
+// get all button with class deleteButton all second
+// declare a variable (objet) and loop on it
+// when i click on one of the delete buttons
+// it delete the li parent of it
 setInterval( () => {
     const deleteButtonElems = document.querySelectorAll("button.deleteButton");
+    console.log(typeof deleteButtonElems)
     for (let button of deleteButtonElems) {
         button.addEventListener("click", function() {
-           this.parentNode.remove()
+           this.parentNode.remove();
         })
     }
 }, 1000);
 
-// When i click on the Clear All button
+// When i click on the Clear All the ul list 
 clearAllButtonElt.addEventListener("click", () => {
     listElt.innerHTML = "";
-})
+});
