a7ba65b7b426fdbef47c6cdde1de425f71c5e5b1
nacim harfouche <nacim_4@hotmail.fr>
2019-08-10 04:17:37 +0200

    add function for remove button, recfactoring and rewrite of how are create and append the elems

--- js/main.js
+++ js/main.js
@@ -3,12 +3,49 @@ const entryElt = document.getElementById("entryWrite"),
 buttonElt = document.getElementById("button"),
 listElt = document.getElementById("list");
 
-// when i click on the button
-buttonElt.addEventListener("click", function() {
-    // get the entry value
-    //entryElt.value
+// create a counter
+let count = 1;
 
+// when i click on the button
+buttonElt.addEventListener("click", () => {
+    // create a li element and set attributes
     let li = document.createElement("li");
-    li.innerHTML = `${entryElt.value} <button type="button" class="deleteButton"><i class="fas fa-times"></i></button>`;
+    //li.id = `myId${count}`;
+
+    // first way how it's possible to do
+    li.textContent = entryElt.value;
+    // create a button 
+    let buttonRemove = document.createElement("button");
+    buttonRemove.type = "button";
+    buttonRemove.classList.add("deleteButton");
+    //buttonRemove.id = count;
+    // create an icon
+    let iconElt = document.createElement("i");
+    iconElt.classList.add("fas", "fa-times");
+
+    // second way to do
+    // li.innerHTML = `${entryElt.value} <button type="button" id="${count}" class="deleteButton"><i class="fas fa-times"></i></button>`;
+
+    // append 
+    // i in button
+    buttonRemove.appendChild(iconElt);
+    // button in li and 
+    li.appendChild(buttonRemove);
+    // li in ul
     listElt.appendChild(li);
+    count++;
 });
+
+// function deleteElemLi(elemId) {
+//     const theDeleteLi = document.querySelector(`ul li#myId${elemId}`);
+//     console.log(theDeleteLi);
+// }
+
+setInterval( () => {
+    const deleteButtonElems = document.querySelectorAll("button.deleteButton");
+    for (let button of deleteButtonElems) {
+        button.addEventListener("click", function() {
+           this.parentNode.remove()
+        })
+    }
+}, 1000);
